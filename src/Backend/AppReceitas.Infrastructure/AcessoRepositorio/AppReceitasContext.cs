399a2515a60321256ab167912c34aaef5ca67e5f
Wesley Fernandes <wesleylfv@hotmail.com>
2023-04-27 23:55:36 -0300

    Criação do contexto e entidade usuario

---
+++ src/Backend/AppReceitas.Infrastructure/AcessoRepositorio/AppReceitasContext.cs
@@ -0,0 +1,17 @@
+﻿using AppReceitas.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppReceitas.Infrastructure.AcessoRepositorio;
+
+public class AppReceitasContext : DbContext
+{
+    public AppReceitasContext(DbContextOptions<AppReceitasContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppReceitasContext).Assembly);
+    }
+}
